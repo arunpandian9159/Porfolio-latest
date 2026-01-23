b727ce4bbe6af87101693ec837e530b68aac470d
Arunpandian C <arunpandiancse25@gmail.com>
2026-01-23 23:37:03 +0530

    feat: enhance Certifications section UI

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -8,7 +8,7 @@ import "./Certifications.css";
 const CertCard = memo(({ cert, index, onImageClick }) => {
   const animationConfig = {
     opacity: [0, 1],
-    rotateY: [45, 0],
+    rotateY: [45, 0], 
     duration: 400, 
     delay: index * 50,
     easing: "easeOutExpo",
