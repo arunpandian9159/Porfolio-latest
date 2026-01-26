41310baefe9babe0b2c8e8cef76e53de39294704
arunpandian9159 <arunpandiancse25@gmail.com>
2026-01-26 21:19:42 +0530

    refactor(Certifications): adjust reveal animation parameters in Certifications component

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -11,7 +11,7 @@ const CertCard = memo(({ cert, index, onImageClick }) => {
     rotateY: [45, 0], 
     duration: 400, 
     delay: index * 50,
-    easing: "easeOutExpo",
+    easing: "easeOutExpo", 
   };
 
   const cardRef = useIntersectionAnimate(animationConfig);
